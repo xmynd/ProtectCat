1600c9ce002995157899a9fe85117b3cc459d342
JordiVila <jordi.vila3@uvic.cat>
2014-02-24 15:26:55 +0100

    Initial Commit

---
+++ ProtectCat/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ProtectCat
+//
+//  Created by jordi.vila3 on 24/02/14.
+//  Copyright (c) 2014 jordi.vila3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ProtectCatTests/ProtectCatTests.m
@@ -0,0 +1,34 @@
+//
+//  ProtectCatTests.m
+//  ProtectCatTests
+//
+//  Created by jordi.vila3 on 24/02/14.
+//  Copyright (c) 2014 jordi.vila3. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ProtectCatTests : XCTestCase
+
+@end
+
+@implementation ProtectCatTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
